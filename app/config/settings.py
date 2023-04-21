4ef3d419af7d253e1916c47802db027e1517f8d9
Ismailov Arsen <aismailov0205@gmail.com>
2023-04-21 18:06:48 +0600

    Refactoring

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", True)))
 
-ALLOWED_HOSTS = [ os.environ.get("DJANGO_ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS", '*')]
 
 CSRF_TRUSTED_ORIGINS = [os.environ.get("CSRF_TRUSTED_ORIGINS")]
 
